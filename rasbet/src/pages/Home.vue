51093e37411b6b3166defa6cec02c5874fc29dab
Rui Santos <ruidsantos99@gmail.com>
2021-11-25 12:35:25 +0000

    add routing and some basic styling - wip

---
+++ rasbet/src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <main-layout>
+    <p>Home Page</p>
+  </main-layout>
+</template>
+
+<script>
+import MainLayout from '../pages/Main.vue'
+export default {
+  components: {
+    MainLayout
+  }
+}
+</script>
